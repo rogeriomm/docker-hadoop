f5f6abc0f2018bf4ac384e755a8c8c1bef3cd6a9
Rogerio Machado <rogermm@gmail.com>
2021-06-28 11:19:50 -0300

    OK

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#
+# FAILED Execution Error return code 2 from org apache hadoop hive ql exec mr MapRedTask
+# https://www.edureka.co/community/66949/failed-execution-error-return-apache-hadoop-hive-mapredtask
+# See file "entrypoint.sh"
+
 set -e
 
 TAG=$(git rev-parse --abbrev-ref HEAD)
@@ -11,7 +16,8 @@ build() {
     IMAGE=$USER/$PREFIX-$NAME:$TAG
     cd $([ -z "$4" ] && echo "./$NAME" || echo "$4")
     echo '--------------------------' building $IMAGE in $(pwd)
-    docker build -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG .
+    echo "--->docker build -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG .<---"
+    docker build --no-cache -t $IMAGE --build-arg USERNAME=$1 --build-arg TAG=$TAG .
     cd -
 }
 
